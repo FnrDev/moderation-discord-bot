86390c0ce2476633eb206809c83c5fb8deeee556
bcoq <ah2014bh@outlook.com>
2020-11-11 06:50:00 +0300

    Added clear command

---
+++ commands/mods/clear.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "clear",
+    run: async(client, message, args) => {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You dont have permission to do this command!')
+
+        if (!args[0]) return message.channel.send('Please enter a amont from 1 to 100')
+        message.delete()
+        let deleteAmont
+        if (parseInt(args[0]) > 100) {
+            deleteAmont = 100;
+        } else {
+            deleteAmont = parseInt(args[0])
+        }
+        await message.channel.bulkDelete(deleteAmont, true)
+        await message.channel.send(`Successfully deleted ${deleteAmont} messages`)
+        .then(m => m.delete({ timeout: 2000}))
+    }
+}
