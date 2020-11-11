5ffd6c2bb1f642b7ed0a90fe7a4e1942ac142009
bcoq <ah2014bh@outlook.com>
2020-11-11 08:05:19 +0300

    Added add-role command

---
+++ commands/mods/add-role.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "addrole",
+    run: async(client, message, args) => {
+        if (!message.member.hasPermission('MANAGE_ROLES')) {
+            return message.channel.send('You dont have permission to do this command!')
+        }
+        const mentionMember = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        if (!mentionMember) {
+            return message.channel.send("Wrong usage `addrole @user`")
+        }
+        if (!args[1]) {
+            return message.channel.send('Please enter role name')
+        }
+        const RoleName = message.guild.roles.cache.find(role => (role.name == args[1])) || (role.id == args[1])
+        const UserAlreadyHaveRole = mentionMember._roles.includes(RoleName.id);
+        if (UserAlreadyHaveRole) {
+            return message.channel.send('User already have this role!')
+        }
+        mentionMember.roles.add(RoleName)
+        .then(message.channel.send(`Successfully added ${RoleName} to ${mentionMember}`))
+        .catch(err => {
+            return message.channel.send('There was an error')
+        })
+    }
+}
