a12a64ebc0f9af7c8ac06b37119a78097e3d0e50
bcoq <ah2014bh@outlook.com>
2020-12-08 02:45:40 +0300

    Added eval command

---
+++ commands/admin/eval.js
@@ -0,0 +1,18 @@
+// You can put more than 2 ids 
+const ownerID = ['596227913209217024', 'OwnerID2']
+
+module.exports = {
+    name: "eval",
+    run: async(client, message, args) => {
+        if (!ownerID.includes(message.author.id)) return
+        let incode = args.join(" ")
+        let code = eval(incode)
+        try {
+            if (typeof incode !== 'string')
+            code = require('util').inspect(code, { depth: 0})
+           message.channel.send(`\`\`\`js\n${code}\n\`\`\``)
+        } catch (e) {
+            message.channel.send(`\`\`\`js\n${e}\n\`\`\``)
+        }
+    }
+}
