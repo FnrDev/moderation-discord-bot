066a786dda0bc7b076969e9e6bd23cb8c5e4eeaf
bcoq <ah2014bh@outlook.com>
2020-12-11 20:30:38 +0300

    Added wasted command

---
+++ commands/extra/wasted.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const axios = require('axios')
+
+module.exports = {
+    name: "wasted",
+    run: async(client, message, args) => {
+        const mentionMember = message.mentions.members.first()
+        const url = `https://some-random-api.ml/canvas/wasted?avatar=${mentionMember.user.displayAvatarURL({ format: 'png'})}`
+
+        let response, data
+        try {
+            response = await axios.get(url)
+            data = response.data
+        } catch (e) {
+            return message.channel.send('There was an error')
+        }
+        message.channel.send({
+            files:
+            [url]
+        })
+    }
+}
