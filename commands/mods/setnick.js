d003550a8a148a288ccf7f4e2cdf1c739a4dddde
bcoq <ah2014bh@outlook.com>
2020-12-11 22:50:17 +0300

    Fixed nickname command

--- commands/mods/setnick.js
+++ commands/mods/setnick.js
@@ -5,12 +5,18 @@ module.exports = {
     category: 'mods',
     description: 'Change nickname for user',
     run: async(client, message, args) => {
-        if (!message.member.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('**⛔ You dont have permission to do this command!**')
+        if (!message.member.hasPermission('MANAGE_NICKNAMES')) {
+            return message.channel.send('**⛔ You dont have permission to do this command!**')
+        }
         const mention = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if (!mention) return message.channel.send('**⛔ You need to mention a user!**')
+        try {
         const nickname = args.slice(1).join(" ")
         if (!nickname) mention.setNickname('')
         if (nickname) await mention.setNickname(nickname)
         return message.channel.send(`**Successfully changed nickname**`)
+        } catch (e) {
+            return message.channel.send('i cant change nickname for this member')
+        }
     }
 }
