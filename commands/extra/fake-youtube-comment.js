436917010ec982ee98e000fb6c0c6a1ee84bb058
bcoq <ah2014bh@outlook.com>
2020-12-11 07:04:56 +0300

    Added fake youtube comment command!

---
+++ commands/extra/fake-youtube-comment.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require('discord.js')
+const axios = require('axios')
+
+module.exports = {
+    name: "yt",
+    run: async(client, message, args) => {
+        const mentionMember = message.mentions.members.first() || message.author
+        let comment = args.join(" ")
+        if (!comment) {
+            return message.channel.send('You need to input your comment')
+        }
+        const url = `https://some-random-api.ml/canvas/youtube-comment?comment=${args.join(" ")}&comment=${comment}&username=${message.author.username}&avatar=${message.author.displayAvatarURL({ format: 'png'})}`
+
+        let response, data
+        try {
+            response = await axios.get(url)
+            data = response.data
+        } catch (e) {
+            return message.channel.send('There was an error please try again')
+        }
+        message.channel.send({
+            files:
+            [url]
+        })
+    }
+}
