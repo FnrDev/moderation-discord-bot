8b27e6fe18959035a161f73da6bc21bb2495735a
bcoq <ah2014bh@outlook.com>
2020-12-11 23:59:25 +0300

    Added custom status

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,12 @@
-module.exports = client => {
-    console.log(`Logged in as ${client.user.username}`)
-}
+module.exports = client => {   
+    let botStatus = [
+      'instagram: @bcoq',
+      'Discord: Fnr#9999',
+      'Github: FnrDev'
+    ]
+    setInterval(function() {
+      let status = botStatus[Math.floor(Math.random() * botStatus.length)]
+      client.user.setActivity(status, {type: 'WATCHING'})
+    }, 20000)  
+      console.log(`Logged in as ${client.user.username}`);  
+    };
